a816e4ee5bf937606818a751d24633df30a87c95
pulitta <pulitta11@gmail.com>
2017-06-28 18:00:02 +0300

    DC-39: invert_operation added

--- src/dmt_domain.erl
+++ src/dmt_domain.erl
@@ -95,22 +95,8 @@ apply_operations(
 -spec revert_operations([operation()], domain()) -> {ok, domain()} | {error, operation_conflict()}.
 revert_operations([], Domain) ->
     {ok, Domain};
-revert_operations([{insert, #'InsertOp'{object = Object}} | Rest], Domain) ->
-    case apply_operations([{remove, #'RemoveOp'{object = Object}}], Domain) of
-        {ok, NewDomain} ->
-            revert_operations(Rest, NewDomain);
-        {error, _} = Error ->
-            Error
-    end;
-revert_operations([{update, #'UpdateOp'{old_object = OldObject, new_object = NewObject}} | Rest], Domain) ->
-    case apply_operations([{update, #'UpdateOp'{old_object = NewObject, new_object = OldObject}}], Domain) of
-        {ok, NewDomain} ->
-            revert_operations(Rest, NewDomain);
-        {error, _} = Error ->
-            Error
-    end;
-revert_operations([{remove, #'RemoveOp'{object = Object}} | Rest], Domain) ->
-    case apply_operations([{insert, #'InsertOp'{object = Object}}], Domain) of
+revert_operations([Operation | Rest], Domain) ->
+    case apply_operations([invert_operation(Operation)], Domain) of
         {ok, NewDomain} ->
             revert_operations(Rest, NewDomain);
         {error, _} = Error ->
@@ -341,3 +327,10 @@ is_reference_type(Type, [{_, _, Type, Tag, _} | _Rest]) ->
     {true, Tag};
 is_reference_type(Type, [_ | Rest]) ->
     is_reference_type(Type, Rest).
+
+invert_operation({insert, #'InsertOp'{object = Object}}) ->
+    {remove, #'RemoveOp'{object = Object}};
+invert_operation({update, #'UpdateOp'{old_object = OldObject, new_object = NewObject}}) ->
+    {update, #'UpdateOp'{old_object = NewObject, new_object = OldObject}};
+invert_operation({remove, #'RemoveOp'{object = Object}}) ->
+    {insert, #'InsertOp'{object = Object}}.
